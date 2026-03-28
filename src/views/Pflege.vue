848ebc2c8137a4493840b4199a0588c6c043db35
Florian Schlenkrich <florian.schlenkrich@ressourcenmangel.de>
2026-03-28 17:10:48 +0100

    update text

--- src/views/Pflege.vue
+++ src/views/Pflege.vue
@@ -3,8 +3,25 @@
 
 
 <template>
-  <div class="home">
+  <div class="rules site-container">
     <router-link class="back-link" to="/">← Zurück</router-link>
 
-  </div>
+
+      <div class="container container--main">
+        <h1>Deckerstellung & Pflege</h1>
+      </div>
+
+      <div class="container">
+        <p>
+
+          Für die Erstellung und Verwaltung eurer Decks nutzen wir Moxfield.<br>
+          Damit die Preisangaben korrekt und vergleichbar bleiben, stellt bitte sicher, dass ihr nach jeder Bearbeitung die Funktion „Update to cheapest“ verwendet. So wird für jede Karte automatisch die günstigste verfügbare Version ausgewählt.<br><br>
+          Bereits vorhandene Decks sind mit dem zentralen Manaknechte-Account verknüpft und aktualisieren sich automatisch, sobald ihr Änderungen in eurem privaten Account vornehmt.<br><br>
+          Wenn ihr neue Decks anlegen oder bestehende löschen möchtet, gebt bitte kurz Florian Bescheid. So kann sichergestellt werden, dass die Decks korrekt im Hauptaccount angelegt und entsprechend verknüpft bzw. entfernt werden.<br><br>
+          Bitte achtet bei der Erstellung eurer Decks immer auf die geltenden Hausregeln. Besonders bei der Wahl des Commanders sollte im Vordergrund stehen, dass der Spielspaß für alle am Tisch erhalten bleibt.<br><br>
+          Decks, an denen ihr noch arbeitet, kennzeichnet bitte im Titel deutlich mit „in Bearbeitung“, damit für alle sichtbar ist, dass diese noch nicht final sind.<br><br>
+
+        </p>
+      </div>
+    </div>
 </template>
